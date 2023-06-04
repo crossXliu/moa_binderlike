e7662cdd2b7ca3f6def378034da6957a6254ea4e
moa <liuweijian_dianqi@163.com>
2023-06-04 14:34:44 +0800

    update demo format, make it as a c class

--- usr-tst/main.c
+++ usr-tst/main.c
@@ -6,6 +6,8 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
 struct moa_binderlike_queue_cap {
   int sq_offset;
@@ -24,61 +26,199 @@ struct moa_binderlike_queue {
   struct moa_binderlike_msg msgs[];
 };
 
+struct moa_binderlike_chan;
+
+typedef int (*dqMsg)(struct moa_binderlike_chan *chan, char *buf, size_t len);
+typedef int (*qMsg)(struct moa_binderlike_chan *chan, char *buf, size_t len);
+
+struct moa_binderlike_chan {
+	int fd;
+	struct moa_binderlike_queue *sq;
+	struct moa_binderlike_queue *cq;
+	struct moa_binderlike_queue_cap cap;
+	dqMsg dequeue;
+	qMsg queue;
+};
+
 #define DEV_NAME "/dev/moa_binderlike"
 #define MOA_BINDERIOC_GET_QUEUE _IOR('B', 0, struct moa_binderlike_queue_cap)
 
-int dq_msg(struct moa_binderlike_queue *q)
+int Msg_Dequeue(struct moa_binderlike_chan *chan, char *buf, size_t sz);
+
+void binderlike_chan_release(struct moa_binderlike_chan *chan)
 {
-	char buf[256];
-	size_t sz;
+	int ret = 0;
+	if (chan)
+	{
+		ret = 0;
+	}
+
+	if (!ret && chan->fd > 0)
+	{
+		if (chan->cap.memblk_size > 0 &&
+			chan->cap.cq_offset > 0)
+		{
+			ret = 0;
+		}
+		else
+		{
+			ret = -ENODEV;
+			close(chan->fd);
+		}
+	}
+
+	if (!ret)
+	{
+		munmap(chan->sq, chan->cap.cq_offset);
+		munmap(chan->cq, chan->cap.memblk_size - chan->cap.cq_offset);
+	}
+
+	if (!ret)
+	{
+		close(chan->fd);
+		free(chan);
+	}
+
+	printf("binderlike release\n");
+}
+
+struct moa_binderlike_chan *binderlike_create_instance(void)
+{
+	int ret = 0;
+	struct moa_binderlike_chan *chan;
+
+	chan = malloc(sizeof(*chan));
+	if (chan)
+	{
+		int fd = -1;
+		memset((void *)chan, 0, sizeof(*chan));
+		fd = open(DEV_NAME, O_RDWR | O_NONBLOCK, S_IRUSR | S_IWUSR);
+		ret = fd > 0 ? 0 : -ENOTTY;
+		chan->fd = fd;
+	}
+
+	if (!ret)
+	{
+		struct moa_binderlike_queue_cap cap;
+		ret = ioctl(chan->fd, MOA_BINDERIOC_GET_QUEUE, &cap);
+		if (ret)
+		{
+			printf("get queue cap failed\n");
+		}
+		else
+		{
+			chan->cap = cap;
+                        printf("record cap %d %d %d\n", cap.sq_offset,
+                               cap.cq_offset, cap.memblk_size);
+                }
+        }
+
+	if (!ret)
+	{
+		void *addr = NULL;
+                addr = mmap(NULL, chan->cap.memblk_size, PROT_READ | PROT_WRITE,
+                            MAP_SHARED, chan->fd, 0);
+                if (addr) {
+                        chan->sq = (struct moa_binderlike_queue *)addr;
+                        printf("sq %p created, h %d, t %d, len %d\n", chan->sq,
+                               chan->sq->head, chan->sq->tail,
+                               chan->sq->total_len);
+
+                        chan->cq = (struct moa_binderlike_queue *)(addr +
+                                   chan->cap.cq_offset);
+                        printf("cq %p created, h %d, t %d, len %d\n", chan->cq,
+                               chan->cq->head, chan->cq->tail,
+                               chan->cq->total_len);
+                } else {
+                        perror("mmap submit queue failed\n");
+			ret = -ENOMEM;
+                }
+        }
+
+
+	if (!ret)
+	{
+		chan->dequeue = Msg_Dequeue;
+		chan->queue = NULL;
+	}
+
+	if (ret < 0)
+	{
+		binderlike_chan_release(chan);
+	}
+
+	return chan;
+}
+
+int dq_msg(struct moa_binderlike_queue *q, char *buf, size_t sz)
+{
+	int ret = 0;
 	if (q->head == q->tail)
-		return -ENOTTY;
+	{
+		ret = -ENOTTY;
+	}
 
-	sz = snprintf(buf, sizeof(buf), "%s", q->msgs[q->head].content);
-	if (buf[sz - 1] == '\n')
-		buf[sz - 1] = '\0';
-	q->head = (q->head + 1) % q->total_len;
-
-	printf("dq result: \"%s\"\n", buf);
+	if (!ret)
+	{
+		sz = snprintf(buf, sz, "%s", q->msgs[q->head].content);
+		if (buf[sz - 1] == '\n')
+			buf[sz - 1] = '\0';
+		q->head = (q->head + 1) % q->total_len;
+	}
 
 	return sz;
 }
 
-int main(int argc, char *argv[]) {
-  int fd = -1;
-  void *addr;
-  struct moa_binderlike_queue_cap cap;
-  struct moa_binderlike_queue *sq, *cq;
-  printf("enter ++\n");
+int qmsg(struct moa_binderlike_queue *q, char *buf, size_t sz)
+{
+	// TODO: to be implememted
+	return 0;
+}
 
-  fd = open(DEV_NAME, O_RDWR | O_NONBLOCK, S_IRUSR | S_IWUSR);
-  if (fd < 0) {
-    printf(" open file %s failed \n", DEV_NAME);
-    return -EFAULT;
-  }
+int Msg_Dequeue(struct moa_binderlike_chan *chan, char *buf, size_t sz)
+{
+	int ret = 0;
+	if (!chan ||
+	    !buf ||
+	    !chan->sq)
+	{
+		ret = -EINVAL;
+	}
 
-  if (ioctl(fd, MOA_BINDERIOC_GET_QUEUE, &cap) < 0) {
-    perror(" ioctl failed\n");
-  }
+	if (!ret)
+	{
+		size_t sz;
+		sz = dq_msg(chan->sq, buf, sz);
+		ret = sz < 0 ? sz : 0;
+	}
 
-  printf(" [sq len %d, cq len %d] \n", cap.cq_offset,
-         cap.memblk_size - cap.cq_offset);
+	return ret;
+}
 
-  addr = mmap(NULL, cap.memblk_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-  if (addr == MAP_FAILED) {
-    perror("mmap failed");
-    close(fd);
-    return -EFAULT;
-  }
+int main(int argc, char *argv[])
+{
+	int ret = 0;
+	struct moa_binderlike_chan *chan;
+	char buf[256];
 
-  sq = (struct moa_binderlike_queue *)(addr + cap.sq_offset);
-  cq = (struct moa_binderlike_queue *)(addr + cap.cq_offset);
+	chan = binderlike_create_instance();
+	if (!chan)
+	{
+		ret = -ENODEV;
+	}
 
-  printf(" sq [%d, %d, %d] cq [%d, %d, %d]\n", sq->head, sq->tail,
-         sq->total_len, cq->head, cq->tail, cq->total_len);
+	if (!ret)
+	{
+		ret = chan->dequeue(chan, buf, sizeof(buf));
+	}
 
-  while (dq_msg(sq) > 0) {
-  }
-  printf("exit --\n");
-  return 0;
+	if (!ret)
+	{
+		printf("dq buf: \"%s\"\n", buf);
+	}
+	else
+	{
+		perror("dequeue msg failed\n");
+	}
+	return ret;
 }
